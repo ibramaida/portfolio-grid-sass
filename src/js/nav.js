8fe118874a634b0fdb8b963bd24a88c6fead1b1b
ibrahim <ibra.maida@protonmail.com>
2021-07-29 23:06:37 +0200

    init portfolio website

---
+++ src/js/nav.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector('.menu-button')
+const nav = document.querySelector('nav')
+
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle("nav-is-open")
+})
+
+nav.addEventListener('click', () => {
+    document.body.classList.remove("nav-is-open")
+})
+
